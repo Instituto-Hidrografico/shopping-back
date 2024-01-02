3349026a48c1226debc5e8e043de66f8a7bfae11
GadelhaTI <gadelha.ti@gmail.com>
2024-01-02 01:02:56 -0300

    add gitignore

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2031,10 +2031,10 @@ INSERT INTO shopping.role(created_at, updated_at, id, name) VALUES(NOW(), NOW(),
 --INSERT INTO shopping.role_privilege(role_id, privilege_id) VALUES ('8652ec73-0a53-433c-93be-420f1d90c681', '3f2146a9-5d43-448e-a1eb-455766d3a14a') ON CONFLICT DO NOTHING;
 
 -- POPULING shopping.user_entity
---INSERT INTO shopping.user_entity(created_at, updated_at, id, username, password, email, active) VALUES (NOW(), NOW(), '6120ce60-4499-4a8d-816b-9080e3f8e6e2', '00038059', '$2a$10$v5q8rJ5T/OlmZ2NKSYB2YOOxkn9AI1K04Bn9pemlEZTAMybsq6ona', 'marcelo.gadelha@marinha.mil.br', true) ON CONFLICT DO NOTHING;
---INSERT INTO shopping.user_entity(created_at, updated_at, id, username, password, email, active) VALUES (NOW(), NOW(), '67d43c5e-bd63-4e17-b386-09438e758194', '00038060', '$2a$10$v5q8rJ5T/OlmZ2NKSYB2YOOxkn9AI1K04Bn9pemlEZTAMybsq6ona', 'gael.gadelha@marinha.mil.br', true) ON CONFLICT DO NOTHING;
---
+INSERT INTO shopping.user_entity(created_at, updated_at, id, username, password, email, active) VALUES (NOW(), NOW(), '6120ce60-4499-4a8d-816b-9080e3f8e6e2', '00038059', '$2a$10$v5q8rJ5T/OlmZ2NKSYB2YOOxkn9AI1K04Bn9pemlEZTAMybsq6ona', 'marcelo.gadelha@marinha.mil.br', true) ON CONFLICT DO NOTHING;
+INSERT INTO shopping.user_entity(created_at, updated_at, id, username, password, email, active) VALUES (NOW(), NOW(), '67d43c5e-bd63-4e17-b386-09438e758194', '00038060', '$2a$10$v5q8rJ5T/OlmZ2NKSYB2YOOxkn9AI1K04Bn9pemlEZTAMybsq6ona', 'gael.gadelha@marinha.mil.br', true) ON CONFLICT DO NOTHING;
+
 ---- POPULING shopping.user_role
---INSERT INTO shopping.user_roles(user_id, role_id) VALUES ('6120ce60-4499-4a8d-816b-9080e3f8e6e2', '7c12004d-e843-4e00-be40-01845ad75834') ON CONFLICT DO NOTHING;
---INSERT INTO shopping.user_roles(user_id, role_id) VALUES ('6120ce60-4499-4a8d-816b-9080e3f8e6e2', '8652ec73-0a53-433c-93be-420f1d90c681') ON CONFLICT DO NOTHING;
---INSERT INTO shopping.user_roles(user_id, role_id) VALUES ('67d43c5e-bd63-4e17-b386-09438e758194', '8652ec73-0a53-433c-93be-420f1d90c681') ON CONFLICT DO NOTHING;
+INSERT INTO shopping.user_roles(user_id, role_id) VALUES ('6120ce60-4499-4a8d-816b-9080e3f8e6e2', '7c12004d-e843-4e00-be40-01845ad75834') ON CONFLICT DO NOTHING;
+INSERT INTO shopping.user_roles(user_id, role_id) VALUES ('6120ce60-4499-4a8d-816b-9080e3f8e6e2', '8652ec73-0a53-433c-93be-420f1d90c681') ON CONFLICT DO NOTHING;
+INSERT INTO shopping.user_roles(user_id, role_id) VALUES ('67d43c5e-bd63-4e17-b386-09438e758194', '8652ec73-0a53-433c-93be-420f1d90c681') ON CONFLICT DO NOTHING;
