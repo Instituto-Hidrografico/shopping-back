d1308b0f0a91ea885228e5ae919c63a34b986f49
1SG-HN Gadelha <marcelo.gadelha@mairnha.mil.br>
2024-01-04 11:10:57 -0300

    refactor: simplify with lambda

--- src/main/java/com/institutohidrografico/shopping/exception/Validator.java
+++ src/main/java/com/institutohidrografico/shopping/exception/Validator.java
@@ -21,18 +21,18 @@ public final class Validator {
         return true;
     }
     public static boolean hasDigit(String value) {
-        return isNull(value)? false : value.chars().anyMatch(letter -> Character.isDigit(letter));
+        return !isNull(value) && value.chars().anyMatch(Character::isDigit);
     }
     public static boolean hasLetter(String value) {
-        return isNull(value)? false : value.chars().anyMatch(letter -> Character.isLetter(letter));
+        return !isNull(value) && value.chars().anyMatch(Character::isLetter);
     }
     public static boolean hasLowerCase(String value) {
-        return isNull(value)? false : value.chars().anyMatch(letter -> Character.isLowerCase(letter));
+        return !isNull(value) && value.chars().anyMatch(Character::isLowerCase);
     }
     public static boolean hasUpperCase(String value) {
-        return isNull(value)? false : value.chars().anyMatch(letter -> Character.isUpperCase(letter));
+        return !isNull(value) && value.chars().anyMatch(Character::isUpperCase);
     }
     public static boolean hasLength(int length, String value) {
-        return isNull(value)? false : value.length() >= length;
+        return !isNull(value) && value.length() >= length;
     }
 }
