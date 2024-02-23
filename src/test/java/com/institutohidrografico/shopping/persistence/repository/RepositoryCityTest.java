f1aef6a03602c83f8f9491da12d1bda567edd313
Gadelha TI <marcelo.gadelha@mairnha.mil.br>
2024-02-23 19:10:11 -0300

    chore: first add test

---
+++ src/test/java/com/institutohidrografico/shopping/persistence/repository/RepositoryCityTest.java
@@ -0,0 +1,34 @@
+package com.institutohidrografico.shopping.persistence.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@DataJpaTest
+class RepositoryCityTest {
+
+    @Test
+    void findByName() {
+    }
+
+    @Test
+    void existsByNameIgnoreCaseAndIdNot() {
+    }
+
+    @Test
+    void existsByName() {
+    }
+
+    @Test
+    void existsByNameIgnoreCase() {
+    }
+
+    @Test
+    void findByIdOrderByIdAsc() {
+    }
+
+    @Test
+    void findByNameContainingIgnoreCaseOrderByNameAsc() {
+    }
+}
