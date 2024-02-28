8c28e4968ed6e7920c77334a0b06dc185aaae924
Gadelha TI <marcelo.gadelha@mairnha.mil.br>
2024-02-28 13:08:23 -0300

    chore: add lines and polygons.

--- src/main/java/com/institutohidrografico/shopping/persistence/model/Location.java
+++ src/main/java/com/institutohidrografico/shopping/persistence/model/Location.java
@@ -6,13 +6,21 @@ import lombok.Data;
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import net.postgis.jdbc.geometry.MultiPolygon;
+import net.postgis.jdbc.geometry.Point;
+import net.postgis.jdbc.geometry.Polygon;
 import org.hibernate.envers.Audited;
-
-import java.awt.*;
+import net.postgis.jdbc.geometry.*;
 
 @Audited @Entity @Data @AllArgsConstructor @NoArgsConstructor @EqualsAndHashCode(callSuper = true)
 public class Location extends GenericEntity {
 
     @Column(columnDefinition = "geography")
     private Point point;
+    @Column(columnDefinition = "geography")
+    private LineString lineString;
+    @Column(columnDefinition = "geography")
+    private Polygon polygon;
+    @Column(columnDefinition = "geography")
+    private MultiPolygon multiPolygon;
 }
