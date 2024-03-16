bf534adc35195311f5b241fba2ae4fe412abc913
GadelhaTI <gadelha.ti@gmail.com>
2024-03-16 00:50:21 -0300

    feature: add all maritime area.

---
+++ src/main/java/com/institutohidrografico/shopping/persistence/model/MaritimeArea.java
@@ -0,0 +1,22 @@
+package com.institutohidrografico.shopping.persistence.model;
+
+import com.institutohidrografico.shopping.persistence.GenericAuditEntity;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.hibernate.envers.Audited;
+import org.locationtech.jts.geom.Polygon;
+
+@Audited @Entity @Data @AllArgsConstructor @NoArgsConstructor @EqualsAndHashCode(callSuper = false)
+public class MaritimeArea extends GenericAuditEntity {
+
+    private String code;
+    private String name;
+    private String start;
+    private String finish;
+    @Column(columnDefinition = "geography")
+    private Polygon polygon;
+}
